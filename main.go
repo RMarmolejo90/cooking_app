69c67fe09f13615fd2142760fc8a265b92fca074
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-07-16 16:26:24 -0400

    resolve errors in main.go

--- main.go
+++ main.go
@@ -7,21 +7,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var db = make(map[string]string)
-
-func setupRouter() *gin.Engine {
-
+func main() {
 	r := gin.Default()
 
 	// Ping test
 	r.GET("/", func(c *gin.Context) {
 		log.Printf("\n\t--- Server is Running ---\n")
 		c.String(http.StatusOK, "server running")
+		return
 	})
-}
-
-func main() {
-	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
