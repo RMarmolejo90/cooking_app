b646a6338269a4e35089cefff26d8f7b8dee841f
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-07-18 14:50:29 -0400

    update recipe handler

--- internal/handlers/recipe.go
+++ internal/handlers/recipe.go
@@ -3,13 +3,10 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/RMarmolejo90/cooking_app/internal/database"
 	"github.com/RMarmolejo90/cooking_app/internal/models"
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus" // or any logger you prefer
+	"github.com/sirupsen/logrus"
 )
-
-var db = database.DB
 
 func CreateRecipe(c *gin.Context) {
 	var recipe models.Recipe
@@ -31,5 +28,18 @@ func CreateRecipe(c *gin.Context) {
 		return
 	}
 
+	c.JSON(http.StatusOK, recipe)
+}
+
+func UpdateRecipe(c *gin.Context) {
+	id := c.Param("id")
+	var recipe models.Recipe
+	if err := db.First(&recipe, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Recipe not found"})
+	}
+
+	if result := db.Save(&recipe); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+	}
 	c.JSON(http.StatusOK, recipe)
 }
