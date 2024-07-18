dfaa81f3215234a60138bdf2b840ae1839e3151c
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-07-18 14:23:09 -0400

    add tag handlers

---
+++ internal/handlers/tag.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+
+)
+
+func CreateTag(c *gin.Context){
+
+}
+
+func GetTag(c *gin.Context){
+
+}
+
+func UpdateTag(c *gin.Context){
+
+}
+
+func DeleteTag(c *gin.Context){
+
+}
+
+func GetPostsByTag(c *gin.Context){
+
+}
+
+func GetRecipesByTag(c *gin.Context){
+
+}
+
