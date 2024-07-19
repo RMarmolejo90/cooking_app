0fa21ee694811129eb0ae05cbfda302ecd45c300
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-07-19 15:39:11 -0400

    add create message handler

--- internal/handlers/message.go
+++ internal/handlers/message.go
@@ -7,3 +7,19 @@ import (
 	"github.com/RMarmolejo90/cooking_app/internal/models"
 	"github.com/gin-gonic/gin"
 )
+
+func CreateMessage(c *gin.Context) {
+	var message models.Message
+
+	if err := c.ShouldBindJSON(&message); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Error binding data to json"})
+		return
+	}
+
+	if result := db.Create(&message); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Server error while saving to the database"})
+		return
+	}
+
+	c.JSON(http.StatusOK, message)
+}
